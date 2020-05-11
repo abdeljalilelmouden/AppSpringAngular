c52794f1972e0183bf4e995727f368525cad755a
abdeljalilelmouden <elmoudene123@gmail.com>
2020-05-12 00:54:20 +0200

    test database POSTGRE

--- src/main/java/com/app/WebServiceRest/FormationWebServiceRest.java
+++ src/main/java/com/app/WebServiceRest/FormationWebServiceRest.java
@@ -1,19 +1,15 @@
 package com.app.WebServiceRest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import com.app.dao.FormationRepository; 
+import org.springframework.web.bind.annotation.RestController; 
 @RestController
 @CrossOrigin("*")
 @RequestMapping(value = "/api/v1")
 public class FormationWebServiceRest {
 	
-	@Autowired
-	private FormationRepository FormationRepository;
-	
+ 
 	@GetMapping(path = "/exportationFormationExcel")
 	public String ExportationFormationExcel(){
 		return "oui";
