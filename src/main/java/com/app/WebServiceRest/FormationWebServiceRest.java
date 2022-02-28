ea0b7e5a56a061d7a9c31412e00a00d695e8ff69
abdeljalil.mouden <elmoudene123@gmail.com>
2022-02-28 21:58:31 +0100

    add method retuen list of formation

--- src/main/java/com/app/WebServiceRest/FormationWebServiceRest.java
+++ src/main/java/com/app/WebServiceRest/FormationWebServiceRest.java
@@ -1,18 +1,33 @@
 package com.app.WebServiceRest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController; 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.dao.FormationRepository;
+import com.app.entities.Formation; 
 @RestController
 @CrossOrigin("*")
 @RequestMapping(value = "/api/v1")
 public class FormationWebServiceRest {
 	
-	
+	@Autowired
+	private FormationRepository formationRepository;
  
 	@GetMapping(path = "/exportationFormationExcel")
 	public String ExportationFormationExcel(){
 		return "oui";
 	}
+	
+	
+	@GetMapping(path = "/test")
+	public List<Formation> list(){
+		return formationRepository.findAll();
+	}
+	
+	
 }
