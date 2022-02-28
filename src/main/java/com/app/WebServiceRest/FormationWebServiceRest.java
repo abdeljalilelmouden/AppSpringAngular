33b9cd614eed81169d0447625146769d649c05b3
abdeljalil.mouden <elmoudene123@gmail.com>
2022-02-28 22:42:16 +0100

    add method retuen list of formation

--- src/main/java/com/app/WebServiceRest/FormationWebServiceRest.java
+++ src/main/java/com/app/WebServiceRest/FormationWebServiceRest.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,10 +28,25 @@ public class FormationWebServiceRest {
 	}
 	
 	
-	@GetMapping(path = "/test")
+	@GetMapping(path = "/list")
 	public List<Formation> list(){
 		return formationRepository.findAll();
 	}
 	
+	@PostMapping(path = "/add")
+	public Formation add(@RequestBody Formation f){
+		return formationRepository.save(f);
+	}
+	
+	
+	@DeleteMapping(path = "/delete")
+	public void delete(@RequestBody Formation f){
+		 formationRepository.delete(f);
+	}
+	
+	@DeleteMapping(path = "/delete/{id}")
+	public void delete(@PathVariable Long id){
+		 formationRepository.getOne(id);
+	}
 	
 }
